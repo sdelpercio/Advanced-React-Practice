86d7f5b349e1ed14ff399d5dbe2282fb04fa75b4
Shawn DelPercio <sdelperc@gmail.com>
2020-02-13 19:19:19 -0500

    refactored app.js into class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  
-    return (
-    <div className="App">
-      <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			players: []
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get('http://localhost:5000/api/footballers')
+			.then(res => {
+				console.log(res);
+				this.setState({ players: res.data });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
+	render() {
+		return <div className='App'></div>;
+	}
 }
 
 export default App;
